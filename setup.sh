f7804647d1ad91cdafdacf94a6a869c444dd532d
Daniel Brouse <me@danielbrouse.com>
2020-02-29 17:57:22 +0800

    first commit

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "setting up database ..."
+cat ./repo/setup/database.sql | sudo mysql -u root
+echo "database created"
+echo "creating db users ..."
+cat ./repo/setup/dbusers.sql | sudo mysql -u root
+echo "users created"
+echo "cleaning up ..."
+rm -rf README.md
+rm -rf ./repo/setup
+mv ./repo/* ./
+rm -rf ./repo
+echo "updating apache ports ..."
+sudo service apache2 stop
+sudo sed -ir '/Listen/{s/\([0-9]\+\)/8080/; :a;n; ba}' /etc/apache2/ports.conf
+echo "updating sites-enabled ..."
+sudo sed -i 's+*:80>+*:8080>+g' /etc/apache2/sites-enabled/000-default.conf
+sudo sed -i 's+DocumentRoot /var/www/html+DocumentRoot /home/ubuntu/environment+g' /etc/apache2/sites-enabled/000-default.conf
+echo "updating apache2.conf ..."
+sudo sed -i 's+<Directory /var/www/>+<Directory /home/ubuntu/environment/>+g' /etc/apache2/apache2.conf
+sudo service apache2 restart
+echo "all done!"
+rm -rf ./setup.sh
---
+++ setup/dbusers.sql
@@ -0,0 +1,2 @@
+CREATE USER IF NOT EXISTS 'student'@'localhost' IDENTIFIED BY 'webdev123';
+GRANT ALL PRIVILEGES ON *.* TO 'student'@'localhost' WITH GRANT OPTION;
